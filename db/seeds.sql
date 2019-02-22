9d8f2948cbdffa02e489bb0a2ec24f51f3e36dfa
tteenz <trantiinaa@yahoo.com>
2019-02-21 18:50:59 -0800

    change

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-use kvp19zgpvnu9rhlu
-
 INSERT INTO burgers (name) VALUES ('Spicy Chicken Sandwich');
 INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
 INSERT INTO burgers (name, devoured) VALUES ('Chicken Bacon Cheeseburger', true);
